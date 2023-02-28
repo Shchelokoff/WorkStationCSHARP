f59ed27a7620df131f4c337847e19578472ba861
Shchelokoff <shchelokoff.max@yandex.ru>
2023-02-28 22:34:28 +0300

    Сделали вторую необязательную задачу

--- seminar2/neobez_zadacha2/Program.cs
+++ seminar2/neobez_zadacha2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите сколько программистов увидит робот: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 100 == 12 || num % 100 == 13 || num % 100 == 14)
+{
+    Console.Write($"{num} программистов");
+}
+else
+{
+    if (num % 10 == 2 || num % 10 == 3 || num % 10 == 4)
+    {
+        Console.Write($"{num} программиста");
+    }
+    else
+    {
+        if (num % 100 == 11)
+        {
+            Console.Write($"{num} программистов");
+        }
+        else if (num % 10 == 1)
+        {
+            Console.Write($"{num} программист");
+        }
+        else Console.Write($"{num} программистов");
+    }
+}
