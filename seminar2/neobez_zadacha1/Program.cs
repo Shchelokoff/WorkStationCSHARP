3d938e2925f5ad43432a193b1cbe2d2446a79798
Shchelokoff <shchelokoff.max@yandex.ru>
2023-02-28 22:39:38 +0300

    Сделали вторую необязательную задачу

---
+++ seminar2/neobez_zadacha1/Program.cs
@@ -0,0 +1,22 @@
+﻿for (int i=1; i<101; i++)
+{
+    if (i%3==0 && i%5==0 && i%15==0)
+    {
+        string j = "FizzBuzz";
+        Console.Write($"{j} ");
+    }
+    else 
+    {
+        if (i%3==0)
+        {
+        string j = "Fizz";
+        Console.Write($"{j} ");
+        }
+        else if (i%5==0)
+        {
+        string j = "Buzz";
+        Console.Write($"{j} ");
+        }
+        else Console.Write($"{i} ");
+    }
+}
