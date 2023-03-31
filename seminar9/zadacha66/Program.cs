e4afb6c27e1dd5a2ce80a1a102fecb77f6dcd0e0
Shchelokoff <shchelokoff.max@yandex.ru>
2023-03-31 19:15:40 +0300

    Сделали домашнее задание для семинара 9

---
+++ seminar9/zadacha66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+m--;
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumMToN(m, n));
+
+int SumMToN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMToN(m, n);
+        return res;
+    }
+}
